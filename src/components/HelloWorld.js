9d664bbf37eeba2b5767da96d2ea931a839b4cb7
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-02-25 16:07:20 -0500

    created the table grid with html and css then refactored with the nested map function. got the post call to the api to return correctly

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,8 +1,52 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class HelloWorld extends Component {
+  state = {
+    board: []
+  }
+
+  componentDidMount() {
+    axios
+      .post('https://minesweeper-api.herokuapp.com/games', {
+        difficulty: 0
+      })
+      .then((resp) => {
+        console.log({ resp })
+        this.setState({
+          board: resp.data.board
+        })
+      })
+  }
+
   render() {
-    return <h1>Hello, World!</h1>
+    return (
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="8">The table header</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.board.map((row) => {
+              return (
+                <tr>
+                  {row.map((col) => {
+                    return (
+                      // <td onClick={}> / *add onclick call to function here to click and play the game - left and right clicks
+                      <td>
+                        <button>{col}</button>
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
+    )
   }
 }
 
